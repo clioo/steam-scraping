093216e9f8c571f084523d3aa93b8f6b720ea78e
clioo <jesus_acosta1996@hotmail.com>
2020-06-03 11:40:09 -0700

    files setup

---
+++ steam/spiders/best_selling.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BestSellingSpider(scrapy.Spider):
+    name = 'best_selling'
+    allowed_domains = ['store.steampowered.com']
+    start_urls = ['https://store.steampowered.com/search/?filter=topsellers/']
+
+    def parse(self, response):
+        pass
